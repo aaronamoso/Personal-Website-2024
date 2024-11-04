25210ae77491eadf03d4a0e10a3fb515710ed8af
Aaron Amoso <amosoaaron@gmail.com>
2024-11-03 20:19:31 -0700

    finished hamburger menu icon

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    // target these elements/containers
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
